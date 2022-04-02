b8c802ac65ef2f92bc9aaef88cc64f41d880bfad
甘春雨 <keyboard3@icloud.com>
2022-04-03 02:06:29 +0800

    fix: 移除掉公共目录

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -5,7 +5,7 @@ export default function Component({ loading }) {
     return (
         <div className='loading'>
             <div className="loading-inner">
-                <img src="/loading.svg" alt="loading" />
+                <img src="loading.svg" alt="loading" />
                 <div>{loading || t('LOADING')}</div>
             </div>
         </div>
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,6 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/sub-player/',
     clean: true
   },
   devServer: {
